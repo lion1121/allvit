c095f6316f557336f48346f0ab314bd6adaf986c
vagrant <vagrant@homestead>
2019-01-21 11:19:46 +0000

    exit ExportCatalog

--- app/ExportCatalog.php
+++ app/ExportCatalog.php
@@ -2,10 +2,10 @@
 
 namespace App;
 
-<<<<<<< HEAD
-=======
+
+
 use App\Helpers\Parsers\XmlParser;
->>>>>>> ae980e1c1e931b4216aaf7f038895e1eae1ef722
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 use \Mavsan\LaProtocol\Interfaces\Import as Import;
@@ -14,8 +14,7 @@ class ExportCatalog extends Model implements Import
 {
     //
     public function import($fileName){
-<<<<<<< HEAD
-=======
+
         if ($fileName){
             $parser = new XmlParser($fileName);
             Log::debug($fileName);
@@ -23,7 +22,6 @@ class ExportCatalog extends Model implements Import
             $parser->parse();
             $parser->writeProducts();
         }
->>>>>>> ae980e1c1e931b4216aaf7f038895e1eae1ef722
         return self::answerSuccess;
     }
 
