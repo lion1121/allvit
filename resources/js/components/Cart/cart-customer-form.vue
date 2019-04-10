0b6837342163b8b71018c6015f985f0a9635d6b7
vagrant <vagrant@homestead>
2019-04-10 13:56:08 +0000

    add cart tabs

---
+++ resources/js/components/Cart/cart-customer-form.vue
@@ -0,0 +1,77 @@
+<template>
+    <form id="customerFrom">
+        <div class="form-group row">
+            <label for="inputName" class="col-sm-2 col-form-label">Имя</label>
+            <div class="col-sm-10">
+                <input v-model="form.name" type="text" class="form-control" id="inputName" placeholder="Имя" required>
+            </div>
+            <p v-if="form.errors.hasOwnProperty('name')"><b>{{form.errors.name}}</b></p>
+        </div>
+        <div class="form-group row">
+            <label for="inputSurname" class="col-sm-2 col-form-label">Фамилия</label>
+            <div class="col-sm-10">
+                <input v-model="form.surname" type="text" class="form-control" id="inputSurname" placeholder="Фамилия"
+                       required>
+            </div>
+            <p v-if="form.errors.hasOwnProperty('surname')"><b>{{form.errors.surname}}</b></p>
+        </div>
+        <div class="form-group row">
+            <label for="inputPhone" class="col-sm-2 col-form-label">Телефон</label>
+            <div class="col-sm-10">
+                <input v-model="form.phone" type="text" class="form-control" id="inputPhone" placeholder="Телефон"
+                       required>
+            </div>
+            <p v-if="form.errors.hasOwnProperty('phone')"><b>{{form.errors.phone}}</b></p>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail" class="col-sm-2 col-form-label">E-mail</label>
+            <div class="col-sm-10">
+                <input v-model="form.email" type="email" class="form-control" id="inputEmail" placeholder="E-mail">
+            </div>
+            <p v-if="form.errors.hasOwnProperty('email')"><b>{{form.errors.email}}</b></p>
+        </div>
+        <div class="form-group">
+            <label for="inputExtra">Дополнительно</label>
+            <textarea v-model="form.extra" class="form-control" id="inputExtra" rows="3"></textarea>
+        </div>
+
+
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary" @click.prevent="checkForm(form)">Продолжить
+                </button>
+            </div>
+        </div>
+    </form>
+
+</template>
+
+<script>
+    export default {
+        name: "cart-customer-form",
+        data() {
+            return {
+                form: {
+                    name: null,
+                    surname: null,
+                    phone: null,
+                    email: null,
+                    extra: null,
+                    errors: []
+                }
+            }
+        },
+        methods: {
+            checkForm() {
+                if (this.form.name && this.form.surname && this.form.phone && this.form.email) {
+                    this.$emit('formValidated',false);
+                }
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
